c959daab0525c17307b8669ed69caaf139e95a23
Alhassane Bah <adbrim99@gmail.com>
2022-12-27 14:55:20 +0000

    first commit

---
+++ src/main/java/com/apibackend/don/ApiBakendApplication.java
@@ -0,0 +1,13 @@
+package com.apibackend.don;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBakendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBakendApplication.class, args);
+    }
+
+}
